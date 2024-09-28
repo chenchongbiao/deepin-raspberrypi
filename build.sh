9b20316090424bc8baec2afdd536dddb5867a354
bluesky <379865549@qq.com>
2024-09-28 22:13:28 +0800

    chore: update build img script
    * 添加工作流自动化构建
    * 添加安装桌面环境和安装器

--- build.sh
+++ build.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
-set -x
+set -xe
 # 不进行交互安装
 export DEBIAN_FRONTEND=noninteractive
-ROOTFS=`mktemp -d`
+BUILD_TYPE="$1"
+ROOTFS="rootfs"
 TARGET_DEVICE=raspberrypi
 ARCH="arm64"
 DISKIMG="deepin-$TARGET_DEVICE.img"
-IMAGE_SIZE=2048
+IMAGE_SIZE=$( [ "$BUILD_TYPE" == "desktop" ] && echo 12288 || echo 2048 )
 COMPONENTS="main,commercial community"
 readarray -t REPOS < ./profiles/sources.list
 PACKAGES=`cat ./profiles/packages.txt | grep -v "^-" | xargs | sed -e 's/ /,/g'`
@@ -16,19 +17,23 @@ PACKAGES=`cat ./profiles/packages.txt | grep -v "^-" | xargs | sed -e 's/ /,/g'`
 sudo apt update -y
 sudo apt-get install -y qemu-user-static binfmt-support mmdebstrap arch-test usrmerge usr-is-merged qemu-system-misc systemd-container
 
-mkdir -p $ROOTFS
 
-# 创建根文件系统
-sudo mmdebstrap \
-    --hook-dir=/usr/share/mmdebstrap/hooks/merged-usr \
-    --skip=check/empty \
-    --include=$PACKAGES \
-    --components="main,commercial,community" \
-    --architectures=${ARCH} \
-    --customize=./profiles/stage-second.sh \
-    beige \
-    $ROOTFS \
-    "${REPOS[@]}"
+
+if [ ! -d "$ROOTFS" ]; then
+    mkdir -p $ROOTFS
+    # 创建根文件系统
+    sudo mmdebstrap \
+        --hook-dir=/usr/share/mmdebstrap/hooks/merged-usr \
+        --skip=check/empty \
+        --include=$PACKAGES \
+        --components="main,commercial,community" \
+        --architectures=${ARCH} \
+        --customize=./profiles/stage-second.sh \
+        beige \
+        $ROOTFS \
+        "${REPOS[@]}"
+fi
+
 
 sudo echo "deepin-$ARCH-$TARGET_DEVICE" | sudo tee $ROOTFS/etc/hostname > /dev/null
 
@@ -57,7 +62,7 @@ sudo mkfs.ext4 "${LOOP}p2" # 根分区 (/)
 
 TMP=`mktemp -d`
 sudo mount "${LOOP}p2" $TMP
-sudo cp -r $ROOTFS/* $TMP
+sudo cp -a $ROOTFS/* $TMP
 
 sudo mount "${LOOP}p1" $TMP/boot
 # 在物理设备上需要添加 cmdline.txt 定义 Linux内核启动时的命令行参数
@@ -65,7 +70,8 @@ PTUUID=$(sudo blkid $LOOP | awk -F'PTUUID="' '{print $2}' | awk -F'"' '{print $1
 echo "console=serial0,115200 console=tty1 root=PARTUUID=$PTUUID-02 rootfstype=ext4 elevator=deadline fsck.repair=yes rootwait quiet init=/usr/lib/raspi-config/init_resize.sh" | sudo tee $TMP/boot/cmdline.txt
 
 # 拷贝引导加载程序/GPU 固件等, 从 https://github.com/raspberrypi/firmware/tree/master/boot 官方仓库中拷贝，另外放入了 cmdline.txt 和 config.txt 配置
-sudo cp -r firmware/* $TMP/boot
+sudo cp -r boot/* $TMP/boot
+sudo cp -a modules/* $TMP/lib/modules/
 
 # 编辑分区表
 PTUUID=$(sudo blkid $LOOP | awk -F'PTUUID="' '{print $2}' | awk -F'"' '{print $1}')
@@ -81,10 +87,45 @@ sudo mount -t sysfs chsys $TMP/sys
 sudo mount -t tmpfs -o "size=99%" tmpfs $TMP/tmp
 sudo mount -t tmpfs -o "size=99%" tmpfs $TMP/var/tmp
 
+function run_command_in_chroot()
+{
+    rootfs="$1"
+    command="$2"
+    sudo chroot "$rootfs" /usr/bin/env bash -e -o pipefail -c "$command"
+}
+
 # 安装树莓派的 raspi-config
-sudo chroot $TMP /usr/bin/env bash -e -o pipefail -c "curl http://archive.raspberrypi.org/debian/pool/main/r/raspi-config/raspi-config_20240313_all.deb -o /tmp/raspi-config.deb"
-sudo chroot $TMP /usr/bin/env bash -e -o pipefail -c "apt update -y && apt install -y /tmp/raspi-config.deb"
-sudo chroot $TMP /usr/bin/env bash -e -o pipefail -c "rm /tmp/raspi-config.deb"
+mkdir -p $TMP/etc/apt/sources.list.d
+# echo "deb [trusted=yes] http://archive.raspberrypi.org/debian/ bookworm main" | sudo tee $TMP/etc/apt/sources.list.d/raspberrypi.list
+curl -L -o $TMP/tmp/raspi-config.deb https://archive.raspberrypi.org/debian/pool/main/r/raspi-config/raspi-config_20240521_all.deb
+run_command_in_chroot "$TMP" "export DEBIAN_FRONTEND=noninteractive && \
+    apt update -y && apt install -y \
+    $TMP/tmp/raspi-config.deb"
+
+if [[ "$BUILD_TYPE" == "desktop" ]];
+then
+    run_command_in_chroot $TMP "export DEBIAN_FRONTEND=noninteractive &&  apt update -y && apt install -y \
+        deepin-desktop-environment-core \
+        deepin-desktop-environment-base \
+        deepin-desktop-environment-cli \
+        deepin-desktop-environment-extras \
+        firefox \
+        deepin-installer \
+        deepin-installer-timezones"
+
+    # 设置安装器
+    sudo install -D ./profiles/deepin-installer.conf $TMP/etc/deepin-installer/deepin-installer.conf
+    echo -n 'apt_source_deb="' | sudo tee -a $TMP/etc/deepin-installer/deepin-installer.conf
+    echo "deb https://community-packages.deepin.com/beige/ beige main commercial community" | sudo tee -a $TMP/etc/deepin-installer/deepin-installer.conf
+    sudo ln -s ../deepin-installer-first-boot.service $TMP/usr/lib/systemd/system/multi-user.target.wants/deepin-installer-first-boot.service
+    sudo rm $TMP/usr/lib/systemd/system/deepin-installer.service
+
+fi
+
+sudo rm $TMP/etc/apt/sources.list.d/raspberrypi.list
+
+# 删除 root 的密码
+sudo sed -i 's/^root:[^:]*:/root::/' $TMP/etc/shadow
 
 sudo umount -l $TMP
 sudo losetup -D $LOOP
